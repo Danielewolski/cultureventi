6a4537e2a6f6ddd230213084b223c31bb5350a0c
Danielewolski <danielewolski02@gmail.com>
2023-03-13 16:03:00 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $('.partners-logos').slick({
-        slidesToShow: 6,
+        slidesToShow: 4,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 1500,
